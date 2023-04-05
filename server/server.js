d3bd07efa91f92daef3c231a0552e252ccff889c
andrii-grynik <andrii.grynik@gmail.com>
2023-04-05 16:46:47 -0600

    "express server,custom middleware and whitelist"

--- server/server.js
+++ server/server.js
@@ -1,7 +1,40 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require('cors'); 
+const { logger } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
+
+//custom middleware logger
+app.use(logger);
+
+// we can create a whitelist for cors access. 
+// whitelist will prevent other websites to acces your backend, so make sure to whitelist your frontend in order to only limit it to one source
+// const  whitelist = ['your site','more websites', 'and even more sites'];
+// const corsOptions = {
+//  origin: (origin, callback) => {
+//    if (whitelist.indexOf(origin) !== -1) {
+        //callback(null, true)
+    //} else {
+        //callbackify(new Error('Not Allowed by CORS'));
+    //}
+  //},
+  //optionsSuccessStatus: 200
+//}
+app.use(cors(/*corsOptions*/)); // uncomment to turn on whitelisting 
+
+
+
+
+
+//to handle url encoded data. 
+app.use(express.urlencoded({ extended: false}));
+
+//built in middleware for json
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   //to set index.html as a root destiination. Either would work
